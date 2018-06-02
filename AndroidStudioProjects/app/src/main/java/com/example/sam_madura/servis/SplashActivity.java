1d290d0faae4c7ce6c57f59c48e0377a2de42a3f
Nanang Budi <na2ng24@gmail.com>
2018-06-02 19:08:45 +0700

    splashscreen

---
+++ AndroidStudioProjects/app/src/main/java/com/example/sam_madura/servis/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.sam_madura.servis;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run (){
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(), login.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+    }
+
